a598e3eb22e4da91e928c273e3ed2c87a5d3ed08
= <singhalsakshi2811@gmail.com>
2023-03-06 13:12:27 +0530

    added try catch

--- config/db.js
+++ config/db.js
@@ -3,12 +3,19 @@
 var MongoClient = require('mongodb').MongoClient;
 var uri = "mongodb://garbageDetection:garbageDetection@ac-wft4fad-shard-00-00.wtk4fvb.mongodb.net:27017,ac-wft4fad-shard-00-01.wtk4fvb.mongodb.net:27017,ac-wft4fad-shard-00-02.wtk4fvb.mongodb.net:27017/?ssl=true&replicaSet=atlas-10r4ta-shard-0&authSource=admin&retryWrites=true&w=majority";
 const connectDB = async() => {
-
-    MongoClient.connect(uri, function(err, client) {
-        const collection = client.db("major_project").collection("devices");
-        // perform actions on the collection object
-        client.close();
-      });
+    try{
+        MongoClient.connect(uri, function(err, client) {
+            const collection = client.db("major_project").collection("devices");
+            // perform actions on the collection object
+            client.close();
+          });
+    }
+    catch(err)
+    {
+        console.log(err)
+        process.exit(1)
+    }
+    
 }
 
 module.exports = connectDB
