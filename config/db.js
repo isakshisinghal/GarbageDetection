7b5b7d4f5e709e2d5fb689db9db772da0e32a849
= <singhalsakshi2811@gmail.com>
2023-03-09 22:43:12 +0530

    maybe final change

--- config/db.js
+++ config/db.js
@@ -1,11 +1,11 @@
 
 
 var MongoClient = require('mongodb').MongoClient;
-var uri = "mongodb://sakshi:sakshi123@ac-kplccvc-shard-00-00.llwhadc.mongodb.net:27017,ac-kplccvc-shard-00-01.llwhadc.mongodb.net:27017,ac-kplccvc-shard-00-02.llwhadc.mongodb.net:27017/?ssl=true&replicaSet=atlas-9l8hv3-shard-0&authSource=admin&retryWrites=true&w=majority";
+var uri = "mongodb://sakshi:sakshi123@ac-r51p2dw-shard-00-00.ship0kf.mongodb.net:27017,ac-r51p2dw-shard-00-01.ship0kf.mongodb.net:27017,ac-r51p2dw-shard-00-02.ship0kf.mongodb.net:27017/?ssl=true&replicaSet=atlas-10s78e-shard-0&authSource=admin&retryWrites=true&w=majority";
 const connectDB = async() => {
     try{
         MongoClient.connect(uri, function(err, client) {
-            const collection = client.db("major_project").collection("signUp");
+            const collection = client.db("garbage").collection("signUp");
             // perform actions on the collection object
             client.close();
           });
