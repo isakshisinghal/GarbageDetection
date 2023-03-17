1b67deb1b79b1b49f089d86170a11d20758e6715
= <singhalsakshi2811@gmail.com>
2023-03-17 20:16:32 +0530

    database changes

--- config/db.js
+++ config/db.js
@@ -1,21 +1,41 @@
 
 
-var MongoClient = require('mongodb').MongoClient;
-var uri = "mongodb://sakshi:sakshi123@ac-r51p2dw-shard-00-00.ship0kf.mongodb.net:27017,ac-r51p2dw-shard-00-01.ship0kf.mongodb.net:27017,ac-r51p2dw-shard-00-02.ship0kf.mongodb.net:27017/?ssl=true&replicaSet=atlas-10s78e-shard-0&authSource=admin&retryWrites=true&w=majority";
+// var MongoClient = require('mongodb').MongoClient;
+// var uri = "mongodb://sakshi:sakshi123@ac-r51p2dw-shard-00-00.ship0kf.mongodb.net:27017,ac-r51p2dw-shard-00-01.ship0kf.mongodb.net:27017,ac-r51p2dw-shard-00-02.ship0kf.mongodb.net:27017/?ssl=true&replicaSet=atlas-10s78e-shard-0&authSource=admin&retryWrites=true&w=majority";
+// const connectDB = async() => {
+//     try{
+//         MongoClient.connect(uri, function(err, client) {
+//             const collection = client.db("garbage").collection;
+//             // perform actions on the collection object
+//             client.close();
+//           });
+//     }
+//     catch(err)
+//     {
+//         console.log(err)
+//         process.exit(1)
+//     }
+    
+// }
+
+// module.exports = connectDB
+
+
+const mongoose = require('mongoose');
+const uri = "mongodb+srv://sakshi:sakshi123@cluster0.ship0kf.mongodb.net/garbage?retryWrites=true&w=majority";
+//const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 const connectDB = async() => {
-    try{
-        MongoClient.connect(uri, function(err, client) {
-            const collection = client.db("garbage").collection;
-            // perform actions on the collection object
-            client.close();
+    
+        mongoose.connect(uri)
+          .then((conn)=>{
+              console.log(conn);
+              console.log("Database connection successful")
+          }).catch((error)=>{
+            console.log(error)
           });
-    }
-    catch(err)
-    {
-        console.log(err)
-        process.exit(1)
-    }
+    
     
 }
-
 module.exports = connectDB
+
+
